a1032ee93fd3c9d4600bebbe59165da453ccea3f
Notemat <155073892+Notemat@users.noreply.github.com>
2024-07-03 15:05:19 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
 from reviews.models import Review, Title
-from .serializers import ReviewSerializer
+from api.serializers import ReviewSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
