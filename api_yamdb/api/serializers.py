e9472739c7ef7026409d0ce3a02405afe4ca2d86
Nikita Kozlov <kozlov.nik@icloud.com>
2024-07-11 16:21:25 +0300

    review&comtent_v_1.03

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -16,6 +16,7 @@ from reviews.models import (Category,
                             User
                             )
 
+
 class CategorySerializer(serializers.ModelSerializer):
     """Серилизатор для модели категорий."""
 
