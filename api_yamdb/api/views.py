6d66f673072d4182c06b471945292a0de8c7b541
Nikita Kozlov <kozlov.nik@icloud.com>
2024-07-11 22:16:38 +0300

    adminpermission_v_1.04

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -229,3 +229,4 @@ class UserViewSet(NotAllowedPutMixin, viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
