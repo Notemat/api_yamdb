3740b66854a09de7fba5a444e2fbc6cc1442ab7a
Nikita Kozlov <kozlov.nik@icloud.com>
2024-07-14 13:43:38 +0300

    constants_v_1.0

--- api_yamdb/reviews/constants.py
+++ api_yamdb/reviews/constants.py
@@ -6,3 +6,12 @@ MIN_SCORE_VALUE = 1
 
 MAX_SCORE_VALUE = 10
 """Максимальная оценка"""
+
+USERNAME_MAX_LENGTH = 150
+"""Максимальная длина username"""
+
+FIELD_MAX_LENGTH = 256
+"""Максимальная длина поля"""
+
+EMAIL_MAX_LENGTH = 254
+"""Максимальная длина email"""
